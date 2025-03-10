61c07254a2f1d96db429da619ca16f4abc959532
Wridzer <wridzer12@gmail.com>
2025-03-10 17:08:48 +0100

    init project

---
+++ src/main/java/koelekikkers/frikandelspeciaal/FrikandelSpeciaalClient.java
@@ -0,0 +1,11 @@
+package koelekikkers.frikandelspeciaal;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FrikandelSpeciaalClient implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world! (client)");
+    }
+    
+}
